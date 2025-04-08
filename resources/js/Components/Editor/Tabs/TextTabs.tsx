35f844e72856935660381baa94e584e683d2e3bf
1001001010 <lll100100@yandex.ru>
2025-04-08 10:34:47 +0800

    Перерендеринг компонента изменения текстового файла

--- resources/js/Components/Editor/Tabs/TextTabs.tsx
+++ resources/js/Components/Editor/Tabs/TextTabs.tsx
@@ -1,16 +1,15 @@
 import { File, PageProps } from '@/types'
 import { Textarea } from '@/Components/ui/textarea'
 import { Button } from '@/Components/ui/button'
-import { RotateCcw } from 'lucide-react'
-import { useState } from 'react'
-import { useForm } from '@inertiajs/react'
+import { ChevronLeft, RotateCcw, Save } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { Link, useForm } from '@inertiajs/react'
 
 export default function TextTabs({ file }: PageProps<{ file: File }>) {
     const { data, setData, post, errors, processing, reset } = useForm({
         fileText: file.content || '',
     })
 
-    const [initialContent, setInitialContent] = useState(file.content)
     const [content, setContent] = useState(file.content)
 
     const restoreText = () => {
@@ -23,24 +22,53 @@ export default function TextTabs({ file }: PageProps<{ file: File }>) {
         post(route('file.edit.upload', { file: file.id }))
     }
 
+    useEffect(() => {
+        setContent(file.content)
+        setData('fileText', file.content ?? '')
+    }, [file.content])
+
     return (
-        <>
-            <div className="flex h-full flex-col space-y-4">
+        <div className="fixed inset-0 z-50 flex flex-col overflow-hidden bg-background">
+            <div className="flex items-center justify-between border-b p-4">
+                <div className="flex items-center gap-2">
+                    <Link href={route('index')}>
+                        <Button variant="ghost" size="icon" className="mr-2">
+                            <ChevronLeft className="h-4 w-4" />
+                            <span className="sr-only">Назад</span>
+                        </Button>
+                    </Link>
+                    <h2 className="text-lg font-semibold">
+                        Редактирование{' '}
+                        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                            {file.name + '.' + file.extension.extension}
+                        </code>
+                    </h2>
+                </div>
+            </div>
+            <div className="w-full flex-1 overflow-hidden">
                 <Textarea
                     placeholder="Содержимое документа"
                     value={data.fileText}
                     onChange={(e) => setData('fileText', e.target.value)}
-                    className="min-h-[400px] flex-1 p-4 md:min-h-[700px] lg:min-h-[700px]"
+                    className="h-full w-full flex-1 p-4"
                 />
+            </div>
 
-                <div className="flex items-center space-x-2">
-                    <Button onClick={saveText}>Сохранить</Button>
-                    <Button variant="secondary" onClick={restoreText}>
-                        <span className="sr-only">Сбросить</span>
-                        <RotateCcw />
+            <div className="flex items-center justify-between border-t bg-background p-4">
+                <div className="flex space-x-2">
+                    <Button onClick={saveText} className="gap-1">
+                        <Save size={18} />
+                        <span>Сохранить</span>
+                    </Button>
+                    <Button
+                        variant="secondary"
+                        onClick={restoreText}
+                        className="gap-1">
+                        <RotateCcw size={18} />
+                        <span>Сбросить</span>
                     </Button>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
