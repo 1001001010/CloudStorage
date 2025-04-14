3d3928a4bcff27cee172d14f23c8c11aba6ee8c6
1001001010 <lll100100@yandex.ru>
2025-04-14 13:02:39 +0800

    Обновление файлового пространства при загрузке через кнопку в SideBar

--- resources/js/Components/Sidebar/FileUploadButton.tsx
+++ resources/js/Components/Sidebar/FileUploadButton.tsx
@@ -3,10 +3,12 @@ import { Upload } from 'lucide-react'
 import { Button } from '@/Components/ui/button'
 import { PageProps } from '@/types'
 import { useForm } from '@inertiajs/react'
+import { useFilesStore } from '@/store/use-file-store'
 
 export default function FileUploadButton({
     open,
 }: PageProps<{ open: boolean }>) {
+    const { currentFolderId, setCurrentPath } = useFilesStore()
     const { data, setData, post } = useForm({
         folder_id: null,
         files: null as File[] | null,
@@ -21,10 +23,24 @@ export default function FileUploadButton({
     useEffect(() => {
         if (data.files && data.files.length > 0) {
             post(route('file.upload'), {
-                onSuccess: () => setData('files', null),
+                onSuccess: async () => {
+                    setData('files', null)
+                    try {
+                        const response = await fetch(
+                            `/api/folder/${currentFolderId}/contents`
+                        )
+                        const updatedData = await response.json()
+                        setCurrentPath([updatedData])
+                    } catch (error) {
+                        console.error(
+                            'Ошибка при обновлении содержимого папки после удаления:',
+                            error
+                        )
+                    }
+                },
             })
         }
-    }, [data.files]) // useEffect следит за изменением data.files
+    }, [data.files])
 
     return (
         <div>
